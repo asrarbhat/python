3839ca13ae0388c205638a874af8d1a842cc9cd2
asrar bhat <asrarpsi@gmail.com>
2021-09-20 00:38:52 +0530

    implement stack

---
+++ data-structures/stack.py
@@ -0,0 +1,34 @@
+class stack:
+    def __init__(self):
+        self.__stack = []
+        self.__length = 0
+
+    def push(self, item):
+        self.__length += 1
+        self.__stack.append(item)
+        return True
+
+    def pop(self):
+        if self.__length == 0:
+            print("stack empty")
+            return False
+        self.__length -= 1
+        return self.__stack.pop()
+
+    def isempty(self):
+        return self.__length == 0
+
+    def print_stack(self):
+        print(self.__stack)
+
+
+if __name__ == "__main__":
+    my_stack = stack()
+    my_stack.pop()
+    my_stack.push(10)
+    my_stack.push(20)
+    my_stack.push(30)
+    my_stack.pop()
+    my_stack.pop()
+    print(my_stack.isempty())
+    my_stack.print_stack()
